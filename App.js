7f1781ee498c9c3b05c759fac7a71165f8efc762
Esca2217 <Estefanicarol.3b3@gmail.com>
2024-04-01 12:07:15 -0300

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Title>01/04/2024 Lado A </Title>
     </View>
   );
 }
---
+++ scr/componentes/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View,Text} from "react-native";
+export default function Title (){
+
+    return(
+    <View>
+        <Text>ONEBITHEALTH</Text>
+        <Text>01/04/2024 3B3 Lado A</Text>
+    </View>
+    );
+}
